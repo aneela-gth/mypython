fa2761e8ecfc8d68d97c7b9eea9aa1e490ce3b0d
aneela-gth <aneelabandi9951@gmail.com>
2025-09-22 23:28:53 +0530

    deepcopy1.py

---
+++ deepcopy1.py
@@ -0,0 +1,61 @@
+#sholow
+import copy
+info={'name':'aneela','city':'hyd','age':23}
+x=copy.copy(info)
+x1=copy.copy(info)
+x['name']='anee'
+print(x)
+print(x1)
+
+import copy 
+score_board={'score':{'runs':135,'wikkets':4,'overs':4.4}}
+shanu=copy.copy(score_board)
+aneela=copy.copy(score_board)
+aneela['score']['runs']+=6
+print(score_board)
+print(shanu)
+print(aneela)
+
+import copy
+x=[[1,2,3],[4,5],6]
+shallow=copy.copy(x)
+
+
+#deeppcopy
+import copy
+info={'detils':{'name':'tharun','city':'hyd','age':23}}
+x=copy.deepcopy(info)
+y=copy.deepcopy(info)
+info['detils']['name']='naveen'
+print(info)
+print(x)
+print(y)
+
+import copy
+pubg={'score':{'kill':0,'health':100}}
+aneela=copy.deepcopy(pubg)
+suppu=copy.deepcopy(pubg)
+sharanya=copy.deepcopy(pubg)
+sreevani=copy.deepcopy(pubg)
+aneela['score']['kill']+=4
+sreevani['score']['health']-=25
+print(pubg)
+print(suppu)
+print(aneela)
+print(sreevani)
+print(sharanya)
+
+import copy
+x=[[1,2,3],[4,5],6]
+shallow=copy.deepcopy(x)
+
+# Fibonacci series using recursion
+def fib(n):
+    if n <= 1:
+        return n
+    return fib(n-1) + fib(n-2)
+
+n = int(input("Enter number of terms: "))
+print("Fibonacci series:")
+for i in range(n):
+    print(fib(i), end=" ")
